ec7941de9ce8997f2647ada4dd8fb3c7601e5ec2
Adrian Estevez <adrianeea.ae@gmail.com>
2024-12-18 00:10:08 -0400

    Use just necessary contracts

--- packages/passagejs/src/codegen/passage3d/claim/v1beta1/tx.amino.ts
+++
@@ -1,8 +0,0 @@
-import { MsgClaim } from "./tx";
-export const AminoConverter = {
-  "/passage3d.claim.v1beta1.MsgClaim": {
-    aminoType: "/passage3d.claim.v1beta1.MsgClaim",
-    toAmino: MsgClaim.toAmino,
-    fromAmino: MsgClaim.fromAmino
-  }
-};
--- packages/passagejs/types/codegen/passage3d/claim/v1beta1/tx.amino.d.ts
+++
@@ -1,8 +0,0 @@
-import { MsgClaim } from "./tx";
-export declare const AminoConverter: {
-    "/passage3d.claim.v1beta1.MsgClaim": {
-        aminoType: string;
-        toAmino: (message: MsgClaim) => import("./tx").MsgClaimAmino;
-        fromAmino: (object: import("./tx").MsgClaimAmino) => MsgClaim;
-    };
-};
